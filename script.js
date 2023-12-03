d52e9582f8d59a18f6baad1b11d53ff19cb0493d
WelsonAA <georgeezwelson@outlook.com>
2023-12-03 21:54:05 +0200

    Project Done

--- script.js
+++ script.js
@@ -58,22 +58,6 @@ function navigateTo(page) {
     window.location.href = page;
 }
 
-function countdownTimer() {
-    const countDownDate = new Date('Dec 31, 2023 23:59:59').getTime(); // Change to your desired end date
-    const x = setInterval(function() {
-        const now = new Date().getTime();
-        const distance = countDownDate - now;
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
-        if (distance < 0) {
-            clearInterval(x);
-            console.log('Countdown finished');
-        }
-    }, 1000);
-}
 
 function updateYear() {
     const yearElement = document.querySelector('.footer p');
